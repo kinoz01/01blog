5fcd9bfc5e6ec1a3af41ccc99cac60187d74cfe9
kino <ayoub-ammar@protonmail.com>
2026-03-09 12:00:00 +0100

    okok

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -5,5 +5,4 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideHttpClient()]
 };
